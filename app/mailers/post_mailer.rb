a173f52aa67f6b69d9eebd5b2e8eb727a190513a
shamoto <thebicameralmind9@gmail.com>
2018-12-18 08:54:36 +0000

    エラー画面リファクタリング

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,6 @@
 class PostMailer < ApplicationMailer
     def post_mailer(post)
         @post=post
-      mail to: @post.user.email, subject:"お問い合わせの確認メール"
+      mail to: @post.user.email, subject: "投稿内容の確認メール"
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    validates  :content, presence: true, length: {in: 1..140}
+    validates :image, presence: true
     belongs_to :user 
     has_many :favorites, dependent: :destroy 
     has_many :favorite_users, through: :favorites, source: :user
